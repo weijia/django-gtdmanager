33d27e5b3d9da071d60a34951b6c0bb1d53abec9
Juraj Komačka <komackaj@gmail.com>
2014-01-11 17:02:29 +0100

    Add ItemForm to inbox view

---
+++ gtdmanager/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from gtdmanager.models import Item
+
+class ItemForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        kwargs['instance'] = Item()
+        super(ItemForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
+        
+    class Meta:
+        model = Item
+        exclude = ('status',)
--- gtdmanager/views.py
+++ gtdmanager/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.template import RequestContext
+
+from gtdmanager.forms import ItemForm
 
 def home(request):
     return HttpResponseRedirect(reverse('gtdmanager:next'))
 
 def inbox(request):
-    return render_to_response('gtdmanager/inbox.html', {'btnName': 'inbox'})
+    show_form = False
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = ItemForm()
+            HttpResponseRedirect(reverse('gtdmanager:inbox'))
+        else:
+            show_form = True
+    else:
+        form = ItemForm()
+
+    return render_to_response('gtdmanager/inbox.html', context_instance=RequestContext(request), 
+        dictionary={ 'btnName': 'inbox', 'itemform': form, 'show_form' : show_form })
 
 def next(request):
     return render_to_response('gtdmanager/next.html', {'btnName': 'next'})
